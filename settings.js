4719b15d71e660bf951089aeab3310c55896be4e
Cyberflashmodz <109969004+Cyberflashmodz@users.noreply.github.com>
2022-07-25 20:36:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,16 +61,16 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = ['94761346248']
+global.ownertag = '94761346248' //ur tag number
+global.botname = '🍁𝗖𝗬𝗕𝗘𝗥 𝗙𝗟𝗔𝗦𝗛🍁' //ur bot name
+global.linkz = "https://youtube.com/channel/UCFrWWVF-8F67JX3Ei34rlrg" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.botscript = 'https://youtube.com/channel/UCFrWWVF-8F67JX3Ei34rlrg' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "සයිබර් ෆ්ලැශ් තමා"හ් හුත්තො" //ur sticker watermark packname
+global.author = "⏤͟͞ ❮🍁𝗖𝗬𝗕𝗘𝗥 𝗙𝗟𝗔𝗦𝗛🍁❯ ͟͞⏤" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
